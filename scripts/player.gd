adca063ca5202bd60d8ec9c4854a37f979a513be
Douglas Kosvoski <douglas.contactpro@gmail.com>
2019-10-13 18:28:37 -0300

    mecanicas refatoradas

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,20 +3,42 @@ extends KinematicBody2D
 onready var anim = get_node("Sprite/AnimatedSprite")
 var motion = Vector2()
 
-const CONST_YSPEED = 1200
-const ACCELERATION = 1.5
-const GRAVITY = 50
-const XSPEED = 400
-var yspeed = CONST_YSPEED
+# define some constants to the player
+const XSPEED = 400 			# horizontal speed
+const YSPEED = 1200			# vertical speed
+const ACCELERATION = 1.5	# jump acceleration
+const GRAVITY = 50			# 'vertical' force pushing down
 
+
+# this var will be modified on the jump function
+var yspeed = YSPEED
+
+var temp_roll = 0
+var temp_atk = 0
 var move = false
 var dir = 'right'
 var jump = false
 var roll = false
-var temp_roll = 0
-var temp_atk = 0
 var atk = false
 
+### Main loop
+func _physics_process(delta):
+	# apply gravity to the player
+	_apply_gravity(GRAVITY)
+	# set player horizontal movement back to X (otherwise it won't stop)
+	motion.x = 0
+	# call the function to handle the animations
+	_move_and_animation()
+
+	# update Vector2 with new positions
+	motion = move_and_slide(motion, Vector2(0,-1))
+
+
+# apply gravity to player on the Y-axis
+func _apply_gravity(force):
+	motion.y += force
+
+# when direction key are pressed move player on the X-axis
 func _move_left_right(dir, motionX, speed):
 	if dir == 'left':
 		motionX -= speed
@@ -24,11 +46,13 @@ func _move_left_right(dir, motionX, speed):
 		motionX += speed
 	return motionX
 
+# when jump key is pressed apply force and acceleration on Y-axis
 func _jump(motionY, speed, gravity, acceleration):
 	motionY = -speed
 	yspeed -= (gravity * acceleration)
 	return motionY
 
+# when attack key is pressed play attack animation
 func _attack(anim, atk, temp_atk):
 	anim.play('attack')
 	if temp_atk <= 20:
@@ -37,13 +61,28 @@ func _attack(anim, atk, temp_atk):
 			atk = false
 		temp_atk += 1	
 	return [atk, temp_atk]
+
+# when roll key is pressed play animation and move player forward
+func _roll(anim, dir, motionX, temp_roll, roll):
+	if is_on_floor() == true:
+		anim.play('roll')
+		if dir == 'left':
+			motion.x -= XSPEED
+		elif dir == 'right':
+			motion.x += XSPEED
+		temp_roll += 1
+		if temp_roll == 30:
+			temp_roll = 0
+			roll = false
+	return [roll, temp_roll]
 	
+# function responsible to control animations and sequences
 func _move_and_animation():
-	if jump:
+	if jump == true:
 		if yspeed > 0:
 			motion.y = _jump(motion.y, yspeed, GRAVITY, ACCELERATION)
 		else:
-			yspeed = CONST_YSPEED
+			yspeed = YSPEED
 			jump = false
 
 	if move == true:
@@ -55,17 +94,10 @@ func _move_and_animation():
 		atk = value[0]
 		temp_atk = value[1]
 		
-	elif roll:
-		if is_on_floor() == true:
-			anim.play('roll')
-			if dir == 'left':
-				motion.x -= XSPEED
-			elif dir == 'right':
-				motion.x += XSPEED
-			temp_roll += 1
-			if temp_roll == 30:
-				temp_roll = 0
-				roll = false
+	elif roll == true:
+		var value = _roll(anim, dir, motion.x, temp_roll, roll)
+		roll = value[0]
+		temp_roll = value[1]
 
 	else:
 		if Input.is_action_pressed('left'):
@@ -100,14 +132,3 @@ func _move_and_animation():
 					jump = true
 			else:
 				anim.play("idle original fat")
-func _apply_gravity(force):
-	motion.y += force
-
-func _physics_process(delta):
-	_apply_gravity(GRAVITY)
-	motion.x = 0
-
-	_move_and_animation()
-
-
-	motion = move_and_slide(motion, Vector2(0,-1))
