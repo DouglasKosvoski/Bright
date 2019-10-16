faee4381dd2e1aca67ccaaa286b17ca9a26828d1
Douglas Kosvoski <douglas.contactpro@gmail.com>
2019-10-16 13:07:56 -0300

    removido intro scene

--- scripts/mainmenu2intro.gd
+++ scripts/mainmenu2intro.gd
@@ -4,7 +4,7 @@ func _ready():
 	pass
 
 func _on_start_button_pressed():
-	get_tree().change_scene('res://Scenes/INTRO.tscn')
+	get_tree().change_scene('res://Scenes/MAIN.tscn')
 
 func _on_quit_button_pressed():
 	get_tree().quit()
